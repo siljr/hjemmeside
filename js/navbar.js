9b3df42d95ca8ad40dba08509ba0ca5c3d2bebd7
Silje Riseng <silje.riseng@gmail.com>
2018-01-08 21:08:46 +0100

    working front page and most content put in html

---
+++ js/navbar.js
@@ -0,0 +1,36 @@
+document.write('\
+  <link rel="stylesheet" type="text/css" href="../css/mainStyle.css" />\
+  <!-- start code for navbar -->\
+  <!-- her har jeg brukt http://www.w3schools.com/css/css_navbar.asp  -->\
+  <nav>\
+  	<div class="window">\
+          <div class="menu-item-container">\
+              <div class="mainpage-links">\
+                  <a id="home" href="index.html" class="circle"><img src="img/homeIcon.svg"></a>\
+                  <a href="index.html" class="startpage-text">Hjem</a>\
+              </div>\
+              <div class="mainpage-links">\
+                  <a id="about" href="about.html" class="circle"><img src="img/aboutIcon.svg"></a>\
+                  <a href="about.html" class="startpage-text">Om meg</a>\
+              </div>\
+              <div class="mainpage-links">\
+                  <a id="cv" href="cv.html" class="circle"><img src="img/CVIcon.svg"></a>\
+                  <a href="cv.html" class="startpage-text">CV</a>\
+              </div>\
+              <div class="mainpage-links">\
+                  <a id="portfolio" href="portfolio.html" class="circle"><img src="img/portfolioIcon.svg"></a>\
+                  <a href="portfolio.html" class="startpage-text">Portefølje</a>\
+              </div>\
+              <div class="mainpage-links">\
+                  <a id="contact" href="contact.html" class="circle"><img src="img/contactIcon.svg"></a>\
+                  <a href="contact.html" class="startpage-text">Kontaktinfo</a>\
+              </div>\
+          </div>\
+      </div>\
+  </nav>\
+  <!-- end code for navbar -->\
+  \
+');
+
+
+//include to html from http://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
