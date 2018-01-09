346f163a40f508ed7ebcdcf35b2a14b7bceff4fd
Silje Riseng <silje.riseng@gmail.com>
2018-01-09 23:37:57 +0100

    top navbar made with selection animation

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,25 @@
 document.write('\
   <link rel="stylesheet" type="text/css" href="../css/mainStyle.css" />\
+  <link rel="stylesheet" type="text/css" href="../css/navstyle.css" />\
   <!-- start code for navbar -->\
   <!-- her har jeg brukt http://www.w3schools.com/css/css_navbar.asp  -->\
   <nav>\
-  	<div class="window">\
+      <div class="window">\
           <div class="menu-item-container">\
               <div class="mainpage-links">\
-                  <a id="home" href="index.html" class="circle"><img src="img/homeIcon.svg"></a>\
-                  <a href="index.html" class="startpage-text">Hjem</a>\
+                  <a id="home" href="index.html" class="circle"><img src="../img/homeIcon.svg"></a>\
               </div>\
               <div class="mainpage-links">\
-                  <a id="about" href="about.html" class="circle"><img src="img/aboutIcon.svg"></a>\
-                  <a href="about.html" class="startpage-text">Om meg</a>\
+                  <a id="about" href="about.html" class="circle"><img src="../img/aboutIcon.svg"></a>\
               </div>\
               <div class="mainpage-links">\
-                  <a id="cv" href="cv.html" class="circle"><img src="img/CVIcon.svg"></a>\
-                  <a href="cv.html" class="startpage-text">CV</a>\
+                  <a id="cv" href="cv.html" class="circle"><img src="../img/CVIcon.svg"></a>\
               </div>\
               <div class="mainpage-links">\
-                  <a id="portfolio" href="portfolio.html" class="circle"><img src="img/portfolioIcon.svg"></a>\
-                  <a href="portfolio.html" class="startpage-text">Portefølje</a>\
+                  <a id="portfolio" href="portfolio.html" class="circle"><img src="../img/portfolioIcon.svg"></a>\
               </div>\
               <div class="mainpage-links">\
-                  <a id="contact" href="contact.html" class="circle"><img src="img/contactIcon.svg"></a>\
-                  <a href="contact.html" class="startpage-text">Kontaktinfo</a>\
+                  <a id="contact" href="contact.html" class="circle"><img src="../img/contactIcon.svg"></a>\
               </div>\
           </div>\
       </div>\
