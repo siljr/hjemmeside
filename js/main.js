65670b8ec918d118c96eb62d522ead787ddfd8eb
Silje Riseng <silje.riseng@gmail.com>
2018-01-13 04:54:00 +0100

    2 second delay added for onclick - not finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("Hei");
+
+$(".circle").click(function (e) {
+    e.preventDefault();                   // prevent default anchor behavior
+    var goTo = this.getAttribute("href"); // store anchor href
+    setTimeout(function(){
+         window.location = goTo;
+    },2000);
+});
